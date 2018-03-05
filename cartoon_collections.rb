7d70b700f81d928a370c7d342f0e0e30ffd59ac2
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-05 21:15:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index do |el, i|
+    puts "#{i}. #{el}"
+  end 
 end
 
 def summon_captain_planet# code an argument here
