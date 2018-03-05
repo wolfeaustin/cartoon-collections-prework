fc815ac9a45f7621d83730bbcf9a1f93c21f4cea
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-05 21:18:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(list)
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  
+  calls.maps{|c| c.upcase << "!"}
 end
 
 def long_planeteer_calls# code an argument here
